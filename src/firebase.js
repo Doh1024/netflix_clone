b4e9c0757607be270196032b78e9d555a6e76017
Doh Kim <dohyeongkim001024@gmail.com>
2023-11-15 17:33:19 -0700

    .env file tweaks

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyBuXqVHYZpURapAYnQaVCmhvEAG4CCraiA",
   authDomain: "netflix-clone-82148.firebaseapp.com",
   projectId: "netflix-clone-82148",
@@ -10,17 +10,17 @@ const firebaseConfig = {
   messagingSenderId: "1048936338282",
   appId: "1:1048936338282:web:6a06316226abffdf0c4b13",
   measurementId: "G-QPYCBV8ZLG"
-};
-
-/*const firebaseConfig = {
-  apiKey: `${process.env.APIKEY}`,
-  authDomain: `${process.env.AUTH_DOMAIN}`,
-  projectId: `${process.env.PROJECT_ID}`,
-  storageBucket: `${process.env.STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
-  appId: `${process.env.APP_ID}`,
-  measurementId: `${process.env.MEASUREMENT_ID}`
 };*/
+
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_APIKEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
